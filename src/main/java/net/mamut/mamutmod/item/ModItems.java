6ec79a8db616f834e8c2e5caf8c6184c3fdeafbc
mamut <mahmut18102008@gmail.com>
2022-09-27 20:48:11 +0200

    Tools added

--- src/main/java/net/mamut/mamutmod/item/ModItems.java
+++ src/main/java/net/mamut/mamutmod/item/ModItems.java
@@ -1,7 +1,7 @@
 package net.mamut.mamutmod.item;
 
 import net.mamut.mamutmod.MamutMod;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,6 +17,28 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
     public static final RegistryObject<Item> Mamutininium = ITEMS.register("mamutininium",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutinium_Sword = ITEMS.register("mamutinium_sword",
+            () -> new SwordItem(ModTiers.Mamutinium, 9, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutinium_Pickaxe = ITEMS.register("mamutinium_pickaxe",
+            () -> new PickaxeItem(ModTiers.Mamutinium, 6, -2.8f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutinium_Axe = ITEMS.register("mamutinium_axe",
+            () -> new AxeItem(ModTiers.Mamutinium, 12, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutinium_Shovel = ITEMS.register("mamutinium_shovel",
+            () -> new ShovelItem(ModTiers.Mamutinium, 6.5f, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutinium_Hoe = ITEMS.register("mamutinium_hoe",
+            () -> new HoeItem(ModTiers.Mamutinium, 2, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/mamut/mamutmod/item/ModTiers.java
+++ src/main/java/net/mamut/mamutmod/item/ModTiers.java
@@ -5,10 +5,10 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier Mamutinium = new ForgeTier(5, 2561, 12,
-            5f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier Mamutinium = new ForgeTier(5, 2561, 15f,
+            0f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.Mamutinium.get()));
-    public static final ForgeTier Mamutininium = new ForgeTier(6, 5000, 15,
+    public static final ForgeTier Mamutininium = new ForgeTier(6, 5000, 1.2f,
             8f, 30, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.Mamutininium.get()));
 }
