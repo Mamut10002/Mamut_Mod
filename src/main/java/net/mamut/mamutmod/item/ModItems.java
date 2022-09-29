8442d785ab4225796e3308bdb4a7b5ada98b67c9
mamut <mahmut18102008@gmail.com>
2022-09-29 20:08:40 +0200

    Mamutininium Tools added

--- src/main/java/net/mamut/mamutmod/item/ModItems.java
+++ src/main/java/net/mamut/mamutmod/item/ModItems.java
@@ -22,19 +22,15 @@ public class ModItems {
     public static final RegistryObject<Item> Mamutinium_Sword = ITEMS.register("mamutinium_sword",
             () -> new SwordItem(ModTiers.Mamutinium, 9, -2.4f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Pickaxe = ITEMS.register("mamutinium_pickaxe",
             () -> new PickaxeItem(ModTiers.Mamutinium, 6, -2.8f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Axe = ITEMS.register("mamutinium_axe",
             () -> new AxeItem(ModTiers.Mamutinium, 12, -3f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Shovel = ITEMS.register("mamutinium_shovel",
             () -> new ShovelItem(ModTiers.Mamutinium, 6.5f, -3f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Hoe = ITEMS.register("mamutinium_hoe",
             () -> new HoeItem(ModTiers.Mamutinium, 2, 0f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
@@ -42,17 +38,30 @@ public class ModItems {
     public static final RegistryObject<Item> Mamutinium_Helmet = ITEMS.register("mamutinium_helmet",
             () -> new ArmorItem(ModArmorMaterials.MAMUTINIUM, EquipmentSlot.HEAD,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Chestplate = ITEMS.register("mamutinium_chestplate",
             () -> new ArmorItem(ModArmorMaterials.MAMUTINIUM, EquipmentSlot.CHEST,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_leggings = ITEMS.register("mamutinium_leggings",
             () -> new ArmorItem(ModArmorMaterials.MAMUTINIUM, EquipmentSlot.LEGS,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
-
     public static final RegistryObject<Item> Mamutinium_Boots = ITEMS.register("mamutinium_boots",
             () -> new ArmorItem(ModArmorMaterials.MAMUTINIUM, EquipmentSlot.FEET,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+
+    public static final RegistryObject<Item> Mamutininium_Sword = ITEMS.register("mamutininium_sword",
+            () -> new SwordItem(ModTiers.Mamutininium, 13, -2.4f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+    public static final RegistryObject<Item> Mamutininium_Pickaxe = ITEMS.register("mamutininium_pickaxe",
+            () -> new PickaxeItem(ModTiers.Mamutininium, 7, -2.8f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+    public static final RegistryObject<Item> Mamutininium_Axe = ITEMS.register("mamutininium_axe",
+            () -> new AxeItem(ModTiers.Mamutininium, 15, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+    public static final RegistryObject<Item> Mamutininium_Shovel = ITEMS.register("mamutininium_shovel",
+            () -> new ShovelItem(ModTiers.Mamutininium, 7.5f, -3f,
+                    new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
+    public static final RegistryObject<Item> Mamutininium_Hoe = ITEMS.register("mamutininium_hoe",
+            () -> new HoeItem(ModTiers.Mamutininium, 3, 0f,
                     new Item.Properties().tab(ModCreativeModeTab.MAMUT_TAB)));
 
 
--- src/main/java/net/mamut/mamutmod/item/ModTiers.java
+++ src/main/java/net/mamut/mamutmod/item/ModTiers.java
@@ -8,7 +8,7 @@ public class ModTiers {
     public static final ForgeTier Mamutinium = new ForgeTier(5, 2561, 15f,
             0f, 22, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.Mamutinium.get()));
-    public static final ForgeTier Mamutininium = new ForgeTier(6, 5000, 1.2f,
-            8f, 30, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier Mamutininium = new ForgeTier(6, 5000, 18f,
+            0f, 30, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(ModItems.Mamutininium.get()));
 }
